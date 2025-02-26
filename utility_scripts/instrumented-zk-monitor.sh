b16806442f7e5ebfc2daede32b8557865172a1be
alessandroCassani <cassani0302@gmail.com>
2025-02-26 18:36:04 +0100

    premlimnary version of instrument zk bash. to inset other legolas buil steps

---
+++ utility_scripts/instrumented-zk-monitor.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+BASE_DIR=$(pwd)/..
+
+echo "building zookeeper first"
+
+cd "${BASE_DIR}/zookeeper"
+mvn clean install -DskipTests -Dmaven.test.skip=true
+
+echo "Building and setting up Legolas"
+cd "${BASE_DIR}/legolas"
+mvn clean install -DskipTests
+
+echo "instrumenting zookeeper with Legolas..."
+cd "${BASE_DIR}/legolas
+./bin/legolas.sh analyzer -s conf/zookeeper/3.6.2/analyzer.json -i "${BASE_DIR}/zookeeper"
+
+
+
+echo "starting instrumented zookeeper monitoring..."
+
