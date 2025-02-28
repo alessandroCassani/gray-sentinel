903dbfaaf8d3ca5b204dd60b4cabe624e999c292
alessandroCassani <cassani0302@gmail.com>
2025-02-28 11:29:33 +0100

    cahned eun zk buildind prior

--- utility_scripts/master.sh
+++ utility_scripts/master.sh
@@ -4,6 +4,7 @@ BASE_DIR=$(pwd)
 
 echo "build and run zookeeper..."
 ZK_PID=$(./runZk.sh)
+echo "$ZK_PID"
 
 if [ -z "$ZK_PID" ]; then
     echo "failed to get zookeeper PID"
@@ -14,6 +15,6 @@ echo "zookeeper running with PID: $ZK_PID"
 
 echo "starting perf monitoring ..."
 
-./perf_monitoring.sh "$ZK_PID"
+PERF_STATS=$(./perf_monitoring.sh "$ZK_PID")
 
 #TODO INFLUX
