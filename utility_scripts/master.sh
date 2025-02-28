c78a23aeef735870b8439e4eb01bf61ca855701e
alessandroCassani <cassani0302@gmail.com>
2025-02-28 12:10:05 +0100

    pid file passing

--- utility_scripts/master.sh
+++ utility_scripts/master.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
 BASE_DIR=$(pwd)
+PID_FILE="/tmp/zookeeper.pid"
 
 echo "build and run zookeeper..."
 ZK_PID=$(./runZk.sh)
-echo "$ZK_PID"
 
-if [ -z "$ZK_PID" ]; then
+if [ -z "$PID_FILE" ]; then
     echo "failed to get zookeeper PID"
     exit 1
 fi
 
-echo "zookeeper running with PID: $ZK_PID"
+ZK_PID=$(cat "$PID_FILE")
+echo "ZK_PID received in master.sh: $ZK_PID"
 
-echo "starting perf monitoring ..."
-
-PERF_STATS=$(./perf_monitoring.sh "$ZK_PID")
+./perf_monitoring.sh 
 
 #TODO INFLUX
--- utility_scripts/perf_monitoring.sh
+++ utility_scripts/perf_monitoring.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-ZK_PID=$1
+PID_FILE="/tmp/zookeeper.pid"
 
-if [ -z "$ZK_PID" ]; then
-    echo "no PID found ..."
+if [ -z "$PID_FILE" ]; then
+    echo "no PID file found ..."
     exit 1
 fi
 
+ZK_PID=$(cat "$PID_FILE")
+
 echo "Monitoring ZooKeeper process with PID: $ZK_PID"
 
-stats=$(perf stat -p "$ZK_PID" sleep 5 2>&1)
-
-echo "$stats"
+perf stat -p "$ZK_PID" sleep 5 2>&1 | tee perf_output.log
