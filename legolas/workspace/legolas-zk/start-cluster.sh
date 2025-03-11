0f4afee80b8e34fe59877019be05766df1f7ad71
alessandroCassani <cassani0302@gmail.com>
2025-03-11 10:06:12 +0100

    cancelled Legolas

--- legolas/workspace/legolas-zk/start-cluster.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for i in 1 2 3; do
-  ZOOCFGDIR=/home/alessandro/PGFDS/legolas/workspace/legolas-zk/conf-$i ZOO_LOG_DIR=/home/alessandro/PGFDS/legolas/workspace/legolas-zk/trials/0/logs-$i /home/alessandro/PGFDS/zookeeper/bin/zkServer.sh start
-done
-
