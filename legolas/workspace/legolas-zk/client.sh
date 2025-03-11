b13e7b2a8b49780fbb362672a24ef3cf29cee0ef
alessandroCassani <cassani0302@gmail.com>
2025-03-11 10:06:12 +0100

    cancelled Legolas

--- legolas/workspace/legolas-zk/client.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-java -cp /home/alessandro/PGFDS/legolas/workspace/legolas-zk/conf-1:/home/alessandro/PGFDS/legolas/driver/zookeeper/3.6.2/target/zk_3_6_2-1.0-jar-with-dependencies.jar edu.umich.order.legolas.zk_3_6_2.ZooKeeperGrayClientMain $@
-
