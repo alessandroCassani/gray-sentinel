7509f875dfc6c73ded279bc18ab3d632b237cc65
alessandroCassani <cassani0302@gmail.com>
2025-03-14 15:06:17 +0100

    fabian client launching

---
+++ cloudsuite/benchmarks/web-serving/launch-clients.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if docker ps -q -f name=fabian_client; then
+    echo " faban_client already running."
+    exit 1
+fi
+
+docker run --net=host --name=faban_client cloudsuite/web-serving:faban_client 127.0.0.1 10 --oper=run --steady=300
+
+if [ $? -eq 0 ]; then
+    echo "fabian_client succesfully running."
+else
+    echo "ERROR"
+fi
