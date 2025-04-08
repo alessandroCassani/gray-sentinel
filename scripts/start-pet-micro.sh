18b7c584485d47a923707bead709a9824423bdec
alessandroCassani <cassani0302@gmail.com>
2025-04-09 02:37:06 +0800

    changes general

--- scripts/start-pet-micro.sh
+++ scripts/start-pet-micro.sh
@@ -1,51 +1,50 @@
 #!/bin/bash
 
-# Start Discovery Server
+mkdir -p target
+
 cd ..
-cd "spring-petclinic-microservices/spring-petclinic-discovery-server"
-../mvnw spring-boot:run &
+cd "spring-petclinic-microservices"
+
+# Start Discovery Server
+cd "spring-petclinic-discovery-server"
+nohup java -jar spring-petclinic-discovery-server/target/*.jar --server.port=8761 --spring.profiles.active=chaos-monkey > target/discovery-server.log 2>&1 &
 sleep 10
 
 # Start Config Server
 cd ..
 cd "spring-petclinic-config-server"
-../mvnw spring-boot:run &
+nohup java -jar spring-petclinic-config-server/target/*.jar --server.port=8888 --spring.profiles.active=chaos-monkey > target/config-server.log 2>&1 &
 sleep 10
 
 # Start Customers Service
 cd ..
 cd "spring-petclinic-customers-service"
-../mvnw spring-boot:run &
+nohup java -jar spring-petclinic-customers-service/target/*.jar --server.port=8081 --spring.profiles.active=chaos-monkey > target/customers-service.log 2>&1 &
+
 sleep 10
 
 # Start Vets Service
 cd ..
 cd "spring-petclinic-vets-service"
-../mvnw spring-boot:run &
+nohup java -jar spring-petclinic-vets-service/target/*.jar --server.port=8083 --spring.profiles.active=chaos-monkey > target/vets-service.log 2>&1 &
 sleep 5
 
 # Start Visits Service
 cd ..
 cd "spring-petclinic-visits-service"
-../mvnw spring-boot:run &
+nohup java -jar spring-petclinic-visits-service/target/*.jar --server.port=8082 --spring.profiles.active=chaos-monkey > target/visits-service.log 2>&1 &
 sleep 5
 
 # Start API Gateway
 cd ..
 cd "spring-petclinic-api-gateway"
-../mvnw spring-boot:run &
-sleep 5
-
-# Start Tracing Server
-cd ..
-cd "spring-petclinic-tracing-server"
-../mvnw spring-boot:run &
+nohup java -jar spring-petclinic-api-gateway/target/*.jar --server.port=8080 --spring.profiles.active=chaos-monkey > target/gateway-service.log 2>&1 &
 sleep 5
 
 # Start Admin Server
 cd ..
 cd "spring-petclinic-admin-server"
-../mvnw spring-boot:run &
+nohup java -jar spring-petclinic-admin-server/target/*.jar --server.port=9090 --spring.profiles.active=chaos-monkey > target/admin-server.log 2>&1 &
 
 echo "Services started. Access URLs:"
 echo "- Discovery Server:   http://localhost:8761"
