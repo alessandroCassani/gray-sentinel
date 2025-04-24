4a5f43fef24469eb7310c90609bd30d341e76822
alessandroCassani <cassani0302@gmail.com>
2025-04-24 18:04:21 +0800

    jvm adjustmetns

--- scripts/load-injection/load-container-injection.sh
+++ scripts/load-injection/load-container-injection.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 # Script to run JMeter load test and then call chaos injection script after a delay
 
+#Available Commands:
+#  CodeCacheFilling       Fill up code cache.
+#  OutOfMemoryError       JVM out of memory
+#  cpufullload            Process occupied cpu full load
+#  delay                  delay time
+#  full-gc                JVM full gc
+#  return                 Return the specify value
+#  script                 Dynamically execute custom scripts
+#  threadfull             Specifies that the application thread is soaring
+#  throwCustomException   throw custom exception
+# throwDeclaredException Throw the first declared exception of method
+
 # Use the full absolute path with leading slash
 JMETER_BIN="/opt/apache-jmeter-5.6.3/bin/jmeter" 
 JMETER_TEST_DIR="../../external/petclinic/spring-petclinic-api-gateway/src/test/jmeter"
@@ -8,7 +20,7 @@ JMX_FILE="petclinic_test_plan.jmx" # JMeter test plan file
 RESULTS_FILE="results.jtl" # JMeter results file
 CHAOS_SCRIPT="../failure-injection/container-injection.sh"
 TARGET_SERVICE="api-gateway" # Service to inject failure
-CHAOS_TYPE="cpu" # Type of failure (cpu, memory, network, latency)
+CHAOS_TYPE="cpu" # Type of failure (cpufull, memory, network, latency)
 DELAY_SECONDS=60 # Wait time before injecting failure
 CHAOS_DURATION=420 # Duration of the chaos experiment in seconds
 
